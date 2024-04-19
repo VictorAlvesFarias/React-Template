0baef366cac4fc83686f274e6a58639b48c8a926
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-04-19 17:30:52 -0300

    Code fixes

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,37 +1,19 @@
 import React from 'react'
 import { UseFormRegisterReturn } from 'react-hook-form';
 
-interface TextContainerProps {
-  register?: UseFormRegisterReturn
-  readOnly?: boolean
-  className:string
-  placeholder?:string
-  disabled?:boolean
-  mask?:[RegExp,string]
-}
-
-interface TextVariation extends Omit<TextContainerProps, "className"> {
-
-}
-
-interface TextComponent extends TextVariation{
-  variation?:keyof typeof  inputVariations;
-}
-
-interface LabelContainerProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
-}
-
-interface LabelVariation extends Omit<LabelContainerProps, "className"> {
-
-}
-
-interface LabelComponent extends LabelVariation {
-  variation?:keyof typeof  labelVariations;
+interface DefaultInputComponentProps {
+  readonly?: boolean
+  disabled?: boolean
+  name?: string
+  onChange?: (e: any) => any
+  onBlur?: (e: any) => any
+  ref?:any,
+  value?:any
 }
 
 interface RootContainerProps {
-  className:string
-  children:React.ReactNode
+  className: string
+  children: React.ReactNode
 }
 
 interface RootVariation extends Omit<RootContainerProps, "className"> {
@@ -39,12 +21,150 @@ interface RootVariation extends Omit<RootContainerProps, "className"> {
 }
 
 interface RootComponent extends RootVariation {
-  variation?:keyof typeof  rootVariations;
+  variation?: keyof typeof rootVariations;
+}
+
+function RootContainer(_: RootContainerProps) {
+  return (
+    <div {..._}>
+      {_.children}
+    </div>
+  )
+}
+
+function Root(props: RootComponent) {
+  const Component = rootVariations[props.variation ?? "default"]
+  return <Component {...props} />;
 }
 
 const rootVariations = {
   default: (props: RootVariation) =>
-    <RootContainer {...props} className="flex-col flex relative text-zinc-200" />
+    <RootContainer {...props} className="flex-col flex relative text-zinc-200" />,
+  row: (props: RootVariation) =>
+    <RootContainer {...props} className="flex items-center relative text-zinc-200" />
+}
+
+interface LabelContainerProps {
+  children: React.ReactNode
+  className: string
+}
+
+interface LabelVariation extends Omit<LabelContainerProps, "className"> {
+
+}
+
+interface LabelComponent extends LabelVariation {
+  variation?: keyof typeof labelVariations;
+}
+
+function Label(props: LabelComponent) {
+  const Component = labelVariations[props.variation ?? "default"]
+  return <Component {...props} />;
+}
+
+function LabelContainer(_: LabelContainerProps) {
+  return (
+    <label {..._} />
+  )
+}
+
+const labelVariations = {
+  default: (props: LabelVariation) =>
+    <LabelContainer {...props} className='mb-1 font-semibold px-1 text-zinc-800 text-sm' />,
+  row: (props: LabelVariation) =>
+    <LabelContainer {...props} className='font-semibold px-1 text-zinc-800 text-sm' />,
+}
+
+interface CheckboxContainerProps {
+  config:DefaultInputComponentProps
+  className: {
+    checked:string
+    base:string
+  }
+  children: React.ReactNode
+}
+
+interface CheckboxVariation extends Omit<CheckboxContainerProps, "className" | "checked"> {
+
+}
+
+interface CheckboxComponent extends CheckboxVariation {
+  variation?: keyof typeof checkboxVariations;
+}
+
+function CheckboxContainer(props: CheckboxContainerProps) {
+  return (
+    <label className={(props.config.value ? props.className.checked : props.className.base) + " transition-all cursor-pointer"}>
+      <input
+        {...props.config}
+        type='checkbox'
+        className='hidden'
+      />
+      {props.config.value && props.children}
+    </label>
+  )
+}
+
+function Checkbox(props: CheckboxComponent) {
+  const Component = checkboxVariations[props.variation ?? "default"]
+  return <Component {...props} />;
+}
+
+interface RootContainerProps {
+  className: string
+  children: React.ReactNode
+}
+
+interface RootVariation extends Omit<RootContainerProps, "className"> {
+
+}
+
+interface RootComponent extends RootVariation {
+  variation?: keyof typeof rootVariations;
+}
+
+const checkboxVariations = {
+  default: (props: CheckboxVariation) =>
+    <CheckboxContainer
+      {...props}
+      className={{
+        base:'bg-white w-5 h-5 rounded border-2 border-zinc-600 bg flex items-center justify-center   ',
+        checked:'bg-indigo-600 w-5 h-5 rounded flex items-center justify-center text-white'
+      }}
+    />,
+}
+
+interface TextContainerProps {
+  config:DefaultInputComponentProps
+  className: string
+  placeholder?: string
+  mask?: [RegExp, string]
+}
+
+interface TextVariation extends Omit<TextContainerProps, "className"> {
+
+}
+
+interface TextComponent extends TextVariation {
+  variation?: keyof typeof inputVariations;
+}
+
+function TextContainer(props: TextContainerProps) {
+  return (
+    <input
+      {...props.config}
+      className={props.className}
+      onChange={(e) => {
+        e.target.value = props.mask ? e.target.value.replace(/\D/g, '').replace(props.mask[0], props.mask[1]) : e.target.value
+        props.config.onChange ? props.config.onChange(e) : null
+      }}
+    />
+  )
+}
+
+function Text(props: TextComponent) {
+  const Component = inputVariations[props.variation ?? "default"]
+  return <Component {...props} />;
 }
 
 const inputVariations = {
@@ -56,59 +176,9 @@ const inputVariations = {
     <TextContainer {...props} className="cursor-pointer h-9 rounded border bg-white border-black text-black indent-1 p-1 text-ellipsis overflow-hidden" />,
 }
 
-const labelVariations = {
-  default: (props: LabelVariation) =>
-    <LabelContainer {...props} className='mb-1 font-semibold px-1 text-zinc-800' />,
-}
-
-function LabelContainer(_: LabelContainerProps) {
-  return (
-    <label {..._} />
-  )
-}
-
-function TextContainer(_: TextContainerProps) {
-  const register = { ..._.register }
-  const props = { ..._, register: null }
-
-  return (
-      <input 
-        {...props}  
-        {...register}
-        onChange={(e)=>{ 
-          e.target.value = props.mask? e.target.value.replace(/\D/g, '').replace(props.mask[0],props.mask[1]):e.target.value
-          register.onChange?register.onChange(e):null
-        }}
-      />
-  )
-}
-
-
-function RootContainer(_: RootContainerProps) {
-  return (
-    <div {..._}>
-      {_.children}
-    </div>
-  )
-}
-
-function Label(props: LabelComponent) {
-  const Component = labelVariations[props.variation ?? "default"]
-  return <Component {...props} />;
-}
-
-function Text(props: TextComponent) {
-  const Component = inputVariations[props.variation ?? "default"]
-  return <Component {...props} />;
-}
-
-function Root(props: RootComponent) {
-  const Component = rootVariations[props.variation ?? "default"]
-  return <Component {...props} />;
-}
-
 export default {
   Text,
   Label,
-  Root
+  Root,
+  Checkbox
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,59 +1,75 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { z } from 'zod'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { AuthContext } from '../context/AuthContext'
 import { zodResolver } from "@hookform/resolvers/zod"
 import Input from '../components/Input'
 import { Button } from '../components/Button'
 import { Form } from '../components/Form'
 import { Span } from '../components/Span'
-import { Section } from '../components/Section'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { LucideCheck } from 'lucide-react'
 
 function Login() {
-  const formSchema = z.object({
-    email: z.string().nonempty("Campo Obrigatório").email("E-Mail Inválido"),
-    password: z.string().nonempty("Campo Obrigatório")
+  const [rememberMe, setRemeberMe] = useState(localStorage.getItem("remember-me") == "true")
+  const [loading, setLoading] = useState({
+    login:false
   })
-  const { handleSubmit, formState: { errors }, register } = useForm<z.infer<typeof formSchema>>(
-    {
-      resolver: zodResolver(formSchema),
-    }
-  );
-  const { signIn } = useContext(AuthContext)
+  const context = useContext(AuthContext)
   const navigate = useNavigate()
 
-  async function handleSingIn(data: any) {
-    navigate("/home")
-    // setLoginLoding(true)
-    // await signIn(data)
-    //   .then(() => {
-    //     setLoginLoding(false)
-    //   })
-    //   .catch(() => {
-    //     setLoginLoding(false)
-    //   })
+  const loginSchema = z.object({
+    email: z.string().nonempty("Campo Obrigatório").email("E-Mail Inválido"),
+    password: z.string().nonempty("Campo Obrigatório"),
+  })
+  const loginForm = useForm<z.infer<typeof loginSchema>>(
+    {
+      resolver: zodResolver(loginSchema),
+    }
+  );
+
+  function handleSingIn(data: any) {
+    setLoading({...loading,login:true})
+    context.signIn(data)
+      .then(() => {
+        setLoading({...loading,login:false})
+      })
+  }
+  function handleSetRememberMe(e) {
+    setRemeberMe(!rememberMe)
+    localStorage.setItem("remember-me", String(!rememberMe))
   }
 
   return (
-    <section className="min-h-screen w-full flex items-center justify-center">
-      <Section>
-        <Form onSubmit={handleSubmit(handleSingIn)}>
+    <section className="min-h-screen flex-col w-full flex items-center justify-center gap-6 bg-zinc-100 p-3">
+      <h1 className='font-semibold text-2xl'>Bem vindo</h1>
+      <div className='gap-3 flex flex-col bg-white shadow-sm rounded sm:px-12 px-6 py-12  sm:w-fit w-full   '>
+        <Form onSubmit={loginForm.handleSubmit(handleSingIn)}>
           <Input.Root>
-            <Input.Label >E-Mail</Input.Label>
-            <Input.Text register={register('email')} />
-            <Span variation='error'>{errors.email?.message}</Span>
+            <Input.Label>E-Mail</Input.Label>
+            <Input.Text placeholder='E-Mail' config={loginForm.register('email')} />
+            <Span variation='error'>{loginForm.formState.errors.email?.message}</Span>
           </Input.Root>
           <Input.Root>
             <Input.Label >Senha</Input.Label>
-            <Input.Text mask={[/(\d{2})(\d{4,5})(\d{4})/, '($1) $2-$3']} register={register('password')} />
-            <Span variation='error'>{errors.password?.message}</Span>
+            <Input.Text placeholder='Senha' config={loginForm.register('password')} />
+            <Span variation='error'>{loginForm.formState.errors.password?.message}</Span>
           </Input.Root>
-          <Button>
-            Teste
+          <Button loading={loading.login}>
+            Entrar
           </Button>
         </Form>
-      </Section>
+        <Input.Root variation='row'>
+          <Input.Checkbox config={{ value: rememberMe, onChange: handleSetRememberMe, }}>
+            <LucideCheck className='w-3 h-3' />
+          </Input.Checkbox>
+          <Input.Label variation='row'>Manter Conectado</Input.Label>
+        </Input.Root>
+      </div>
+      <div className='flex gap-1 text-sm flex-wrap'>
+        <p>Não possui uma conta?</p>
+        <Link className='text-indigo-700 font-semibold' to={"/signup"}>Clique aqui</Link>
+      </div>
     </section>
   )
 }
