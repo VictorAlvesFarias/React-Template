1e24e6e54a165041ffa0a1f4cc78054dc49f4a3e
Victor Alves Farias <victor.alves@baraodemaua.br>
2024-06-17 09:51:55 -0300

    Components added and bug fixes

--- src/routers/application.tsx
+++ src/routers/application.tsx
@@ -1,34 +1,38 @@
-import { LucideHome, LucideList, LucideMenu, LucideUser } from 'lucide-react'
+import { Home, LucideHome, LucideList, LucideMenu, LucideUser } from 'lucide-react'
 import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Sidebar from '../components/sidebar'
 import { AuthContext } from '../context/auth-context'
-import Home from '../pages/Home'
+import SidebarRoot from '../components/sidebar-root'
+import SidebarMenu from '../components/sidebar-menu'
+import SidebarItem from '../components/sidebar-item'
+import SidebarHref from '../components/sidebar-href'
+import SidebarHamburguer from '../components/sidebar-hamburguer'
+import SidebarContent from '../components/sidebar-content'
 
 function Application() {
     const {logout} = useContext(AuthContext)
 
     return (
-        <Sidebar.Root>
-            <Sidebar.Menu>
-                <Sidebar.Item href='/home'>
+        <SidebarRoot>
+            <SidebarMenu>
+                <SidebarItem href='/home'>
                     <LucideHome className='w-5 h-5' strokeWidth={1.75} />
-                    <Sidebar.Href>Home</Sidebar.Href>
-                </Sidebar.Item>
-                <Sidebar.Item onClick={logout} href='' variation='spacing-top'>
+                    <SidebarHref>Home</SidebarHref>
+                </SidebarItem>
+                <SidebarItem onClick={logout} href='' variation='spacing-top'>
                     <LucideHome  className='w-5 h-5' strokeWidth={1.75} />
-                    <Sidebar.Href>Exit </Sidebar.Href>
-                </Sidebar.Item>
-            </Sidebar.Menu>
-            <Sidebar.Content>
-                <Sidebar.Hamburguer>
+                    <SidebarHref>Exit </SidebarHref>
+                </SidebarItem>
+            </SidebarMenu>
+            <SidebarContent>
+                <SidebarHamburguer>
                     <LucideMenu className='w-7 h-7 text-zinc-300' />
-                </Sidebar.Hamburguer>
+                </SidebarHamburguer>
                 <Routes>
                     <Route path="/home/*" element={<Home />} />
                 </Routes>
-            </Sidebar.Content>
-        </Sidebar.Root>
+            </SidebarContent>
+        </SidebarRoot>
     )
 }
 
