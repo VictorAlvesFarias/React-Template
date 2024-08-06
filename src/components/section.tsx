8f4fcec0e1db323b475b095862ee5df1edb5075e
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-08-06 17:07:24 -0300

    Added components fixes

---
+++ src/components/section.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector } from '../utils/hooks/selector-hooks'
+
+interface SectionProps {
+  className:string
+  children: React.ReactNode
+}
+
+function SectionContainer(props: SectionProps) {
+  return (
+    <div className={props.className}>
+      {props.children}
+    </div>
+  )
+}
+
+const sectionVariations = {
+  default: (_: SectionProps) =>
+    <SectionContainer {..._} className='max-w-7xl w-11/12 h-full lg:px-0 px-5 py-16' />,
+}
+
+const Section = useSelector<keyof typeof sectionVariations,SectionProps>(sectionVariations)
+
+export default Section
