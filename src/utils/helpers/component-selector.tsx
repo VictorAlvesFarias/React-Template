69f1950f95dde361f3f812d8f81d0dd386c855be
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2025-03-14 17:19:12 -0300

    Removed className omission from component selector

--- src/utils/helpers/component-selector.tsx
+++ src/utils/helpers/component-selector.tsx
@@ -3,10 +3,9 @@ import { forwardRef } from "react";
 function componentSelector<T, K>(components) {
     type Selector = K & {
         variation?: T;
-        ref: any,
-        locked?: boolean
+        ref: any
     };
-    const Data = forwardRef<any, Omit<Selector, "className">>((e, ref) => {
+    const Data = forwardRef<any, Selector>((e, ref) => {
 
         return (
             components[e.variation ?? "default"](e, ref)
